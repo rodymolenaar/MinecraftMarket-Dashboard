b99974dec059f7c7913b9baa407372eedd9c664f
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-05-22 16:39:05 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Paths
+ |--------------------------------------------------------------------------
+ */
+var paths = {
+    'jquery': 'node_modules/jquery/',
+    'bootstrap': 'node_modules/bootstrap-sass/assets/',
+    'fontawesome': 'node_modules/font-awesome/',
+    'mdif': 'node_modules/material-design-iconic-font/'
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Config
+ |--------------------------------------------------------------------------
+ */
+
+elixir.config.assetsPath = 'src';
+elixir.config.publicPath = 'dist';
+
+/*
+ |--------------------------------------------------------------------------
+ | Asset Management
+ |--------------------------------------------------------------------------
+ */
+
+elixir(function(mix) {
+    mix.sass('main.scss', 'dist/css/main.css')
+       .copy(paths.bootstrap + 'fonts/bootstrap/**', 'dist/fonts/bootstrap')
+       .copy(paths.fontawesome + 'fonts/**', 'dist/fonts/font-awesome')
+       .copy(paths.mdif + 'dist/fonts/**', 'dist/fonts/material-design-iconic-font');
+});
+
+elixir(function(mix) {
+    mix.scripts([
+         paths.jquery + 'dist/jquery.js',
+         paths.bootstrap + 'javascripts/bootstrap.js'
+       ], 'dist/js/main.js', './');
+});
