74c84a04b973c4d1fbdf4eb8aef7ae720022caca
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-05-30 22:30:33 +0200

    Ok Apple, I give up. You can keep your bouncy scroll thing

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -5,18 +5,3 @@ $(".menu-toggle").click(function(e) {
   $(".content").toggleClass("sidebar-expanded");
   $(".sidebar").toggleClass("sidebar-open");
 });
-
-document.body.addEventListener('touchmove', function(event) {
-  console.log(event.source);
-  if (event.source == document.body) {
-    event.preventDefault();
-  }
-}, false);
-
-window.onresize = function() {
-  $(document.body).width(window.innerWidth).height(window.innerHeight);
-}
-
-$(function() {
-  window.onresize();
-});
