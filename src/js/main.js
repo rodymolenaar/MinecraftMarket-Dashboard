61c81234f388d00c26c1edf391e14ac1124e6873
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-07-09 15:09:57 +0200

    Fancy dropdowns

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
-var $ = require('jquery')(window);
+var $ = require('jquery');
 global.jQuery = require("jquery");
 require('bootstrap-sass');
+require('bootstrap-select');
 require('justgage');
 require('./sidebar.js');
+
+$(document).ready( function () {
+  $('.selectpicker').selectpicker({
+    iconBase: 'fa',
+    tickIcon: 'fa-check',
+    style: 'btn-select',
+    width: 'fit'
+  });
+});
+
+$(document).ready( function () {
+  $('.selectpicker-lg').selectpicker({
+    iconBase: 'fa',
+    tickIcon: 'fa-check',
+    style: 'btn-select btn-select--lg',
+    width: 'fit'
+  });
+});
